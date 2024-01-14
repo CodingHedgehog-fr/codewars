bf5a745629d9537c2c7da39c2cb02e6c2b204038
CodingHedgeHog-fr <ymoignard@netcourrier.com>
2024-01-14 18:27:30 +0100

    New kata : Strings Mix

--- katas/Kyu_4.py
+++ katas/Kyu_4.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+from itertools import groupby
 import re
 
 
@@ -405,3 +406,73 @@ class RomanNumerals:
 
         return decimal_nb
 
+# ----------------------------------------------------------------------------------------------------------------------
+# Description:
+#
+# Given two strings s1 and s2, we want to visualize how different the two strings are. We will only take into account
+# the lowercase letters (a to z). First let us count the frequency of each lowercase letters in s1 and s2.
+#
+# s1 = "A aaaa bb c"
+# s2 = "& aaa bbb c d"
+#
+# s1 has 4 'a', 2 'b', 1 'c'
+# s2 has 3 'a', 3 'b', 1 'c', 1 'd'
+#
+# So the maximum for 'a' in s1 and s2 is 4 from s1; the maximum for 'b' is 3 from s2. In the following we will not
+# consider letters when the maximum of their occurrences is less than or equal to 1.
+#
+# We can resume the differences between s1 and s2 in the following string: "1:aaaa/2:bbb" where 1 in 1:aaaa stands for
+# string s1 and aaaa because the maximum for a is 4. In the same manner 2:bbb stands for string s2 and bbb because
+# the maximum for b is 3.
+#
+# The task is to produce a string in which each lowercase letters of s1 or s2 appears as many times as its maximum
+# if this maximum is strictly greater than 1; these letters will be prefixed by the number of the string where they
+# appear with their maximum value and :. If the maximum is in s1 as well as in s2 the prefix is =:.
+#
+# In the result, substrings (a substring is for example 2:nnnnn or 1:hhh; it contains the prefix) will be in decreasing
+# order of their length and when they have the same length sorted in ascending lexicographic order (letters and digits -
+# more precisely sorted by codepoint); the different groups will be separated by '/'. See examples and "Example Tests".
+#
+# Hopefully other examples can make this clearer.
+#
+# s1 = "my&friend&Paul has heavy hats! &"
+# s2 = "my friend John has many many friends &"
+# mix(s1, s2) --> "2:nnnnn/1:aaaa/1:hhh/2:mmm/2:yyy/2:dd/2:ff/2:ii/2:rr/=:ee/=:ss"
+#
+# s1 = "mmmmm m nnnnn y&friend&Paul has heavy hats! &"
+# s2 = "my frie n d Joh n has ma n y ma n y frie n ds n&"
+# mix(s1, s2) --> "1:mmmmmm/=:nnnnnn/1:aaaa/1:hhh/2:yyy/2:dd/2:ff/2:ii/2:rr/=:ee/=:ss"
+#
+# s1="Are the kids at home? aaaaa fffff"
+# s2="Yes they are here! aaaaa fffff"
+# mix(s1, s2) --> "=:aaaaaa/2:eeeee/=:fffff/1:tt/2:rr/=:hh"
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def mix(s1: str, s2: str) -> str:
+    # k -> key ('a', 'b', ...)
+    # v -> value (number of occurrences per key)
+    # g -> group
+    # substring means '1:aaa', '2:eeee', '=rr', ...
+
+    s1 = ''.join(sorted([c for c in s1 if c.isalpha() and c.islower()]))
+    s2 = ''.join(sorted([c for c in s2 if c.isalpha() and c.islower()]))
+
+    # build a list of tuples (k, substring, v), sorted by k, filter occurrences > 1 only
+    lst_grp = sorted(([(k, f"1:{k*v}", v) for k, v in {k: len(list(g)) for k, g in groupby(s1)}.items() if v > 1] +
+                      [(k, f"2:{k*v}", v) for k, v in {k: len(list(g)) for k, g in groupby(s2)}.items() if v > 1]))
+
+    # build a filtered list of tuples (substrings, v): keep max occurrences for each letter, substitute '1' or '2'
+    # with '=' in case of equality between s1 and s2
+    lst_max = []
+    for lst in [list(g) for _, g in groupby(lst_grp, lambda x: x[0])]:
+        lst_max.append(('=' + lst[0][1][1:], lst[0][2]) if (len(lst) > 1 and lst[0][2] == lst[1][2])
+                       else max([elt for elt in lst], key=lambda x: x[2])[1:])
+
+    # build final list of substrings only: sort by decreasing order of occurrences, in case of equality of occurrences
+    # sort then by ascending lexicographic order
+    lst_sorted = []
+    for _, g in groupby(sorted(lst_max, key=lambda x: x[1], reverse=True), lambda x: x[1]):
+        lst_sorted += [x[0] for x in sorted(list(g), key=lambda x: x[0])]
+
+    return '/'.join(lst_sorted)
--- tests/test_Kyu_4.py
+++ tests/test_Kyu_4.py
@@ -5,7 +5,8 @@ from katas.Kyu_4 import (strip_comments,
                          User,
                          exp_sum,
                          balanced_parens,
-                         RomanNumerals)
+                         RomanNumerals,
+                         mix)
 
 
 class TestKyu4(TestCase):
@@ -199,3 +200,27 @@ class TestKyu4(TestCase):
         self.assertEqual(RomanNumerals.from_roman('IV'), 4)
         self.assertEqual(RomanNumerals.from_roman('MMVIII'), 2008)
         self.assertEqual(RomanNumerals.from_roman('MDCLXVI'), 1666)
+
+    def test_mix(self):
+
+        self.assertEqual(mix("my&friend&Paul has heavy hats! &", "my friend John has many many friends &"),
+                         "2:nnnnn/1:aaaa/1:hhh/2:mmm/2:yyy/2:dd/2:ff/2:ii/2:rr/=:ee/=:ss")
+        self.assertEqual(mix("mmmmm m nnnnn y&friend&Paul has heavy hats! &",
+                             "my frie n d Joh n has ma n y ma n y frie n ds n&"),
+                         "1:mmmmmm/=:nnnnnn/1:aaaa/1:hhh/2:yyy/2:dd/2:ff/2:ii/2:rr/=:ee/=:ss")
+        self.assertEqual(mix("Are the kids at home? aaaaa fffff", "Yes they are here! aaaaa fffff"),
+                         "=:aaaaaa/2:eeeee/=:fffff/1:tt/2:rr/=:hh")
+        self.assertEqual(mix("Are they here", "yes, they are here"), "2:eeeee/2:yy/=:hh/=:rr")
+        self.assertEqual(mix("Sadus:cpms>orqn3zecwGvnznSgacs","MynwdKizfd$lvse+gnbaGydxyXzayp"),
+                         '2:yyyy/1:ccc/1:nnn/1:sss/2:ddd/=:aa/=:zz')
+        self.assertEqual(mix("looping is fun but dangerous", "less dangerous than coding"),
+                         "1:ooo/1:uuu/2:sss/=:nnn/1:ii/2:aa/2:dd/2:ee/=:gg")
+        self.assertEqual(mix(" In many languages", " there's a pair of functions"),
+                         "1:aaa/1:nnn/1:gg/2:ee/2:ff/2:ii/2:oo/2:rr/2:ss/2:tt")
+        self.assertEqual(mix("Lords of the Fallen", "gamekult"),
+                         "1:ee/1:ll/1:oo")
+        self.assertEqual(mix("codewars", "codewars"), "")
+        self.assertEqual(mix("A generation must confront the looming ", "codewarrs"),
+                         "1:nnnnn/1:ooooo/1:tttt/1:eee/1:gg/1:ii/1:mm/=:rr")
+
+
